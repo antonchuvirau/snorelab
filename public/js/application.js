919059d11f72ad929f3de5ea18b618ac38ff6414
Surkov Pavel <surkovv.pavel@gmail.com>
2021-08-12 10:00:58 +0700

    adds interactivity to m-menu

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,15 @@
 
 'use strict';
 
-// Mobile menu
+// Toggle mobile menu
 const btnOpen = document.querySelector('.m-options__menu-btn');
 const btnClose = document.querySelector('.m-menu__close-btn');
+const mobileMenu = document.querySelector('.m-menu');
+
+btnOpen.onclick = () => {
+  mobileMenu.classList.remove('m-menu_hidden');
+}
+
+btnClose.onclick = () => {
+  mobileMenu.classList.add('m-menu_hidden');
+}
